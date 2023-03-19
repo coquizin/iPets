af6b98bdf775b0b9153ec3271aab7b6434f9b727
Anderson Costa <69824671+GPDA-UFABC@users.noreply.github.com>
2023-03-19 01:30:15 -0300

    fix input

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,7 +1,7 @@
 import { InputProps } from "./types";
 
 export default function Input(props: InputProps) {
-  const { label, type, name, id, placeholder, errors, value } = props;
+  const { label, type, name, id, placeholder, errors } = props;
 
   return (
     <>
@@ -17,7 +17,6 @@ export default function Input(props: InputProps) {
         className={`${
           Boolean(errors) ? "border-red-600" : " border-[#C1CCD6]"
         } w-full max-h-[62px] rounded-md text-base duration-300 p-5 border-[2px] border-solid focus-visible:outline-none `}
-        value={value}
       />
       <div className="self-start text-sm text-red-600">
         {Boolean(errors) && errors?.message}
