3eb5c41b3aa8e63b1da89d06604c5c81f01549ca
Cauli Vilela <88348221+coquizin@users.noreply.github.com>
2023-04-03 08:48:50 -0300

    remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,23 +4,23 @@ import { CookieKey } from "@global-utils/cookies/types";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get(CookieKey.JwtAuthToken);
 
-  if (!token || token.length === 0) {
-    if (req.url.includes("criar-conta/cliente")) {
-      return NextResponse.redirect(`${req.nextUrl.origin}/criar-conta`);
-    }
+  // if (!token || token.length === 0) {
+  //   if (req.url.includes("criar-conta/cliente")) {
+  //     return NextResponse.redirect(`${req.nextUrl.origin}/criar-conta`);
+  //   }
 
-    if (
-      req.url.includes(`perfil`) ||
-      req.url.includes(`historico`) ||
-      req.url.includes(`carteira`) ||
-      req.url.includes(`cupons`) ||
-      req.url.includes(`suporte`)
-    ) {
-      return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-    }
+  //   if (
+  //     req.url.includes(`perfil`) ||
+  //     req.url.includes(`historico`) ||
+  //     req.url.includes(`carteira`) ||
+  //     req.url.includes(`cupons`) ||
+  //     req.url.includes(`suporte`)
+  //   ) {
+  //     return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  //   }
 
-    return;
-  }
+  //   return;
+  // }
 
   return NextResponse.next();
 }
